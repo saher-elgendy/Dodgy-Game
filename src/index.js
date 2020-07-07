9a713579cba18dacb3b8b96eb6864e79237fc378
unknown <saherelgendy1990@gmail.com>
2020-07-07 03:42:38 -0700

    adding playerSize and boardUnit props

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Game />
+    <Game playerSize={25} boardUnit={11} />
   </React.StrictMode>,
   document.getElementById('root')
 );
