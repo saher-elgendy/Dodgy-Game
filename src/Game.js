500009f03664ed01c62af397ccc7b34dd023c45e
unknown <saherelgendy1990@gmail.com>
2020-07-07 03:43:59 -0700

    creating player positions state

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Board from './components/board';
 
-function Game() {
+const Game = ({ playerSize, boardUnit }) => {
+  const maxDim = playerSize * boardUnit;
+  const center = (maxDim - playerSize) / 2;
+
+  const [positions, setPositions] = useState({
+    player: {
+      top: center,
+      left: center
+    },
+    enemies: []
+  })
+
+  
   return (
     <div className="Game">
-     
+      <Board positions={positions} maxDim={maxDim} playerSize={playerSize} />
     </div>
   );
 }
