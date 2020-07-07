81bb9eeb82a0216be06a030452845c5851347c16
unknown <saherelgendy1990@gmail.com>
2020-07-07 03:46:18 -0700

    handling player movement in the game

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Board from './components/board';
 
 const Game = ({ playerSize, boardUnit }) => {
@@ -13,7 +13,43 @@ const Game = ({ playerSize, boardUnit }) => {
     enemies: []
   })
 
-  
+  const handlePlayerMovement = (e) => {
+    let { player: { top, left } } = positions;
+    let newPlayer = {};
+    switch (e.keyCode) {
+      case 37:
+        if (left === 0) return;
+        newPlayer = { top, left: left - playerSize };
+        break;
+      case 38:
+        if (top === 0) return;
+        newPlayer = { left, top: top - playerSize };
+        break;
+      case 39:
+        if (left === maxDim - playerSize) return;
+          newPlayer = { top, left: left + playerSize }
+        break;
+      case 40:
+        if(top === maxDim - playerSize) return;
+        newPlayer = { left, top: top + playerSize };
+        break;
+      default:
+        newPlayer = { top, left }
+    }
+
+    setPositions({
+      ...positions,
+      player: newPlayer
+    })
+  }
+
+  useEffect(() => {
+    window.addEventListener('keydown', handlePlayerMovement);
+    return () => {
+      window.removeEventListener('keydown', handlePlayerMovement);
+    }
+  });
+
   return (
     <div className="Game">
       <Board positions={positions} maxDim={maxDim} playerSize={playerSize} />
